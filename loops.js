7738d18921875b61b2d450d3af7e14d371777c99
afnanalansari <afnan.alansari111@gmail.com>
2023-09-13 19:46:56 +0300

    loops

---
+++ loops.js
@@ -0,0 +1,30 @@
+
+
+
+for (let num = 1; num < 11; num++) {
+    console.log(num);
+}
+
+for (let num = 2; num < 21; num++) {
+    num%2 === 0 ? console.log(num) : num = num
+}
+
+let sum =0;
+let num = 1;
+while (num <= 100) {
+    sum += num;
+    num++;
+}
+console.log(sum);
+
+
+let sum2 =0;
+let num2 = 0;
+const number = [1, 2 ,3 ,4 ,5 ,6];
+
+while (num2 < number.length) {
+    sum2 += number[num2];
+    num2++;
+}
+console.log(sum2);
+
